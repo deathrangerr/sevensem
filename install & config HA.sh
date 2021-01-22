7af92ca890ec80e3defa9f7c6d781f8aeae7c821
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-22 14:46:11 +0530

    Add files via upload

---
+++ install & config HA.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Tell the kernel we'll be using IP's that are not defined in the interfaces file
+sed -i '$ a net.ipv4.ip_nonlocal_bind=1' /etc/sysctl.conf
+sysctl -p
+
+#Install HAPROXY Keepalived
+sudo yum install -y haproxy keepalived
+
+#Configure the Keep alived
+cp /etc/keepalived/keepalived.conf  /root/keepalived.conf.bkp
+sed -i 'd' /etc/keepalived/keepalived.conf
+sudo sh -c 'echo "global_defs { \n  router_id haproxy01\n} \nvrrp_script haproxy { \n  script "killall -0 haproxy" \n  interval 2 \n  weight 2 \n} \nvrrp_instance 50 { \n  virtual_router_id 50 \n  advert_int 1 \n priority 101 \n state MASTER \n interface ens33 \n  virtual_ipaddress { \n    192.168.0.110 dev ens33 \n  } \n track_script { \n    haproxy \n  } \n}" /etc/keepalived/keepalived.conf'
+
+#Start keepalived and Haproxy
+sudo systemctl enable keepalived haproxy && systemctl start haproxy keepalived
