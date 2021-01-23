b31eaa1f9dc62864c40c756f2ed831d0c22cc24b
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-23 23:38:29 +0530

    Update manualdeploy(devops).sh

--- manualdeploy(devops).sh
+++ manualdeploy(devops).sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo apt update
+
+#sudo apt-get update
+sudo apt-get install tomcat7 -y
+sleep 5s
+
+cp /home/ubuntu/sevensem/CloudenabledWebApp/target/CloudenabledWebApp.war /var/lib/tomcat7/webapps/
+
 sudo apt install wget git vim nano curl openjdk-8-jdk maven -y 
 git clone https://github.com/deathrangerr/CloudenabledWebApp.git
 cd CloudenabledWebApp
@@ -9,12 +16,10 @@ mvn package
 
 sed -i '$ a deb http://us.archive.ubuntu.com/ubuntu/ xenial main \ndeb http://us.archive.ubuntu.com/ubuntu/ xenial universe' /etc/apt/sources.list
 
-#sudo apt-get update
-sudo apt-get install tomcat7 -y
-
-cp /home/ubuntu/sevensem/CloudenabledWebApp/target/CloudenabledWebApp.war /var/lib/tomcat7/webapps/
 
 systemctl restart tomcat7
+
+
 
 ###visit public ip of ur instance with port 8080 and project name
 
