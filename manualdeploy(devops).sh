5f6a0caf6db7062a98a579f05c9ccd54c5aae24d
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-23 23:57:04 +0530

    Update manualdeploy(devops).sh

--- manualdeploy(devops).sh
+++ manualdeploy(devops).sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 sudo apt update
 
-#sudo apt-get update
+sudo apt install  openjdk-8-jdk -y
 sed -i '$ a deb http://us.archive.ubuntu.com/ubuntu/ xenial main \ndeb http://us.archive.ubuntu.com/ubuntu/ xenial universe' /etc/apt/sources.list
 sudo apt update
 sudo apt-get install tomcat7 -y
+
 sleep 5s
 
 
-sudo apt install wget git vim nano curl openjdk-8-jdk maven -y 
+sudo apt install wget git vim nano curl maven -y 
 git clone https://github.com/deathrangerr/CloudenabledWebApp.git
 cd CloudenabledWebApp
 mvn compile
