651efc2eea0d1ebbb0031f00243a7c026cb43be6
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-26 00:17:40 +0530

    Update sonar-runner(slavenode).sh

--- sonar-runner(slavenode).sh
+++ sonar-runner(slavenode).sh
@@ -4,4 +4,4 @@ wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scann
 unzip sonar-scanner-cli-4.2.0.1873-linux.zip 
 cd  sonar-scanner-4.2.0.1873-linux/
 sed -i '4 a sonar.host.url=sonarmasterip' conf/sonar-scanner.properties 
-sed -i '$ a /home/ubuntu/sonar-scanner-4.2.0.1873-linux/bin' /home/ubuntu/sonar-scanner-4.2.0.1873-linux/bin
+sed -i '$ a /home/ubuntu/sonar-scanner-4.2.0.1873-linux/bin' vim /etc/environment
