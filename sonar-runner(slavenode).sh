73a6b97725823f8992ac9cca8d7fa1bbfbc0d0af
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-26 00:33:18 +0530

    Update sonar-runner(slavenode).sh

--- sonar-runner(slavenode).sh
+++ sonar-runner(slavenode).sh
@@ -7,5 +7,28 @@ sed -i '4 a sonar.host.url=#sonarmasterip:9000' conf/sonar-scanner.properties
 sed -i '$ a /home/ubuntu/sonar-scanner-4.2.0.1873-linux/bin' /etc/environment
 
 
+##example jenkins pipeline script
+#node{
+#stage('cloning')
+#git'https://github.com/deathrangerr/CloudenabledWebApp.git'
+
+#stage('compile')
+#sh'mvn compile'
+
+#stage('sonar analysis')
+#sh 'mvn sonar:sonar   -Dsonar.projectKey=sevensem   -Dsonar.host.url=http://3.236.208.208:9000   -Dsonar.login=f55b6c2ddc588fa572bc61647b5f80a4472e3867'
+
+
+#stage('test')
+#sh'mvn test'
+
+#stage('package')
+#sh'mvn package'
+
+#stage('copying')
+#sh'sudo cp target/CloudenabledWebApp.war /var/lib/tomcat7/webapps/'
+#}
+
+
 
 
