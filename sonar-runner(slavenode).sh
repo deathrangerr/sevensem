346934e5de7fb2fa25ff1abaab31c6a9556d1b1d
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2021-01-26 00:26:11 +0530

    Update sonar-runner(slavenode).sh

--- sonar-runner(slavenode).sh
+++ sonar-runner(slavenode).sh
@@ -7,7 +7,5 @@ sed -i '4 a sonar.host.url=sonarmasterip' conf/sonar-scanner.properties
 sed -i '$ a /home/ubuntu/sonar-scanner-4.2.0.1873-linux/bin' vim /etc/environment
 
 
-#pipeline script jenkins
-#stage('sonar analysis')
-#sh 'mvn sonar:sonar -Dsonar.projectKey=cloudenabled -Dsonar.host.url=http://ipofmaster  -Dsonar.login=2a9f3e6d25311a992f4798d0e3280590c238f914'
+
 
